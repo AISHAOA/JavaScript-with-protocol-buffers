5d79822e59ba899f44d4e938f650b88afa8e961b
AISHA <57065939+AISHAOA@users.noreply.github.com>
2022-02-03 14:22:31 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+//1.import protobuf package
+const protobuf = require('protobufjs');
 
+//2.load the proto file and the package name
+async function run() {
+     const root = await protobuf.load('filename.proto');
+     const proto = root.lookupType('package.Message');
+  
+//3.encode your data
+  var encodedData = proto.encode({
+                         dataDefineInyourMessage: any.value
+                    }
+                    ).finish();
+//4.decode your data
+    var decodeData = proto.decode(encodedData);
+}
